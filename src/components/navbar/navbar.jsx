908bd4feecec78f294b5731b6eec72ba924f678e
Anxhelika <krecianxhelika04@gmail.com>
2026-02-26 22:08:09 +0100

    Fillimi i projektit ecommerce marrja e gatshme e assets qe do te perdorim dhe krijimi inavbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../assets/logo.png";
+import cart_icon from "../assets/cart_icon.png";
+const Navbar= ()=>{
+    const [menu , setmenu]= React.useState("shop");
+    return(
+        <div className="navbar">
+            <div className="nav-logo">
+                <img src={logo} alt="logo" />
+                <p>SHOPPER</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={()=>setmenu("shop")}>Shop{menu==="shop" && <hr />}</li>
+                <li onClick={()=>setmenu("men")}>Men{menu==="men" && <hr />}</li>
+                <li onClick={()=>setmenu("women")}>Women{menu==="women" && <hr />}</li>
+                <li onClick={()=>setmenu("kids")}>Kids{menu==="kids" && <hr />}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="cart" />
+                <div className="nav-cart-count">0</div>
+
+            </div>
+  
+        </div>
+    )
+}
+export default Navbar;
